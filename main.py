0b0f816f7a5b8dae98231aee62b200415191d25c
Khasanjon <101443665+khasanjon-mamadaliyev@users.noreply.github.com>
2023-08-05 06:16:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -49,45 +49,7 @@ main_link = "https://my.gov.uz/oz/service/415"
 username = 'user'
 password = 'password'
 region = 'Toshkent shahri'
-car_number, seria, tex_number = '01G408JB', 'AAF', '1713736'
+car_number, seria, tex_number = 'car_number', 'seria', 'tex_number'
 
 asyncio.run(login(main_link, username, password, region, car_number, seria, tex_number))
 
-"""
- 01G408JB  | 1713736       | AAF
- 40K583LA  | 0399164       | AAF
- 01W790YA  | 0033408       | AAF
- 01Z177NA  | 1426786       | AAF
- 01626QGA  | 1859259       | AAF
- 01402DFA  | 7453516       | AAC
- 10Y760KA  | 4961127       | AAC
- 01J117DB  | 0476916       | AAF
- 30071TTF  | 6753967       | AAC
- 01K510WA  | 6262147       | AAC
- 01W220FB  | 1296820       | AAF
- 01C017TA  | 5400499       | AAC
- 40B168SA  | 1541769       | AAF
- 10Q003SA  | 1569662       | AAF
- 95470TAA  | 6904182       | AAC
- 10Y049BB  | 1326470       | AAG
- 01M736ZB  | 1090207       | AAG
- 01655BGA  | 0765057       | AAF
- 01D504KB  | 1904866       | AAF
- 01H001200 | 2836764       | AAC
- 01211VSB  | 2150669       | AAF
- 80E661PA  | 1585852       | AAF
- 01H141DA  | 0204020       | AAE
- 010511AA  | 0786685       | AAF
- 70J728LA  | 6838376       | AAC
- 90Z780KA  | 0067060       | AAF
- 95F772KA  | 2755770       | AAF
- 01E088MB  | 2425878       | AAF
- 01H328YA  | 0896527       | AAF
- 30M445VA  | 2740776       | AAF
- 70T444AA  | 2133581       | AAF
- 60757RAA  | 1101224       | AAF
- 01A799YA  | 6777847       | AAC
- 01T793KB  | 2058737       | AAF
- 90Y373MA  | 2528837       | AAF
- 75A938JA  | 7286395       | AAC
-"""
